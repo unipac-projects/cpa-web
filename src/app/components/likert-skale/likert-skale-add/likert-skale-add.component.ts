f7c0f3cc920d0987d79135f5f34a8b32075aaf5c
Iago <iagofalcom@hotmail.com>
2019-11-09 18:57:36 -0300

    Implementing likert-skale-add

--- src/app/components/likert-skale/likert-skale-add/likert-skale-add.component.ts
+++ src/app/components/likert-skale/likert-skale-add/likert-skale-add.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { LikertSkala } from './../../../shared/LikertSkala';
+import { LikertSkalaService } from './../../../services/likert-skala/likert-skale.service';
+import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-likert-skale-add',
@@ -7,9 +11,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LikertSkaleAddComponent implements OnInit {
 
-  constructor() { }
+  @Input() likertskale = new LikertSkala();
+  name: string;
+  likertskaleForm: FormGroup;
+  isLoadingResults = false;
+
+  constructor(private api: LikertSkalaService,
+    private router: Router,
+    private formBuilder: FormBuilder) { }
+
+  onFormSubmit(form:NgForm) {
+    this.isLoadingResults = true;
+
+    const { name  } = this.likertskaleForm.value;
+    this.likertskale = new LikertSkala();
+    this.likertskale.name = name;
+
+    this.api.add(this.likertskale)
+      .subscribe(res => {
+          console.log(res);
+          let id = res['id'];
+          this.isLoadingResults = false;
+          this.router.navigate(['/likertskale-details', id]);
+        }, (err) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        });
+    }
+
+  add() {
+    this.api.add(this.likertskale).subscribe((res) => {
+      this.router.navigate['/likertskale'];
+    }, (err) => {
+        console.log(err);
+    });
+  }
 
   ngOnInit() {
+    this.likertskaleForm = this.formBuilder.group({
+      'name' : [null, Validators.required]
+    });
   }
 
 }
+
