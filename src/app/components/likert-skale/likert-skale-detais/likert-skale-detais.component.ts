38db8d7cbec1c5b4ae67f2cdd566de2b583a5876
Iago <iagofalcom@hotmail.com>
2019-11-09 19:10:08 -0300

    Implementing likert-skale-detais

--- src/app/components/likert-skale/likert-skale-detais/likert-skale-detais.component.ts
+++ src/app/components/likert-skale/likert-skale-detais/likert-skale-detais.component.ts
@@ -1,4 +1,7 @@
+import { LikertSkala } from './../../../shared/LikertSkala';
+import { LikertSkalaService } from './../../../services/likert-skala/likert-skale.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-likert-skale-detais',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LikertSkaleDetaisComponent implements OnInit {
 
-  constructor() { }
+  likertskala = new LikertSkala();
+  id: number;
+  name: string;
+  isLoadingResults = true;
+  constructor(private api: LikertSkalaService,
+              private router: Router,
+              private route: ActivatedRoute) { }
+
+  delete(id: number) {
+    this.api.remove(id)
+      .subscribe(res => {
+        console.log(res);
+        this.router.navigate(['/likertskale']);
+      }, (err) => {
+        console.log(err);
+      }
+    );
+  }
 
   ngOnInit() {
+    let id = this.route.snapshot.params['id'];
+    this.api.getById(id).subscribe((likertskala: LikertSkala) => {
+      console.log(likertskala);
+      this.likertskala = likertskala;
+      this.isLoadingResults = false;
+    });
   }
 
 }
